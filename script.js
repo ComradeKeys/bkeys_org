d87bd6e89adfb7a4b0fac2d71f1443820538b430
ComradeKeys <key13005@byui.edu>
2015-08-22 17:20:56 -0600

    more work done

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-var d = new Date();
-var m = d.getMinutes();
-var h = d.getHours();
-
 var nav_but_mouse_handler  = function() {
     $('.nav_but').mouseover(function() {
 	$(this).fadeTo("slow", 0.5);
@@ -10,40 +6,20 @@ var nav_but_mouse_handler  = function() {
     $('.nav_but').mouseout(function() {
 	$(this).fadeTo("fast", 1.0);
     });
+
+    $('.nav_active').mouseover(function() {
+	$(this).fadeTo("slow", 0.5);
+    });
+
+    $('.nav_active').mouseout(function() {
+	$(this).fadeTo("fast", 1.0);
+    });
 };
 
 /*
-Sets a background according to the time of day
-*/
-var set_theme  = function() {
-
-    var background  = "rgb(255, 255, 255)";
-    var primary     = "rgb(10, 40, 129)";
-    var secondary   = "rgb(255, 174, 0)";
-    var tertiary    = "rgb(13, 17, 254)";
-    var foot_div    = " linear-gradient(to right, rgba(0, 0, 0, 0)," + primary + ", rgba(0, 0, 0, 0))";
-    var nav_bar     = " linear-gradient(to top, " + tertiary + ", " + primary;
-    /*If it is dard outside*/
-    if(h < 7 || h > 20) {
-	background  = "rgb(10, 10, 10)";
-	primary     = "rgb(37, 37, 37)";
-	secondary   = "rgb(72, 72, 72)";
-    }
-
-    $('body').css({ "background-color": background});
-    $('h1').css({ "color": secondary});
-    $('.nav_but').css({ "color": "white"});
-    $('.main_nav').css({ "background-image": nav_bar});
-    $('.nav_but').css({ "color": "white"});
-    $('.foot_divide').css({ "background-image": foot_div});
-};
-
-/*
-Main function of javascript
+  Main function of javascript
 */
 $(document).ready(function() {
 
-    set_theme();
     nav_but_mouse_handler();
-    draw_circle();
 });
